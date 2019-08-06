bda54ed14008ff664b48dc964f162cc5fcfea349
Jeff Stafford <jstafford@vmfarms.com>
2019-08-06 12:39:28 -0400

    migrate documentation from gitbook

---
+++ README.md
@@ -0,0 +1,6 @@
+# ElasticSearch
+
+## Features
+
+This module will deploy a brand new AWS ElasticSearch domain to a VPC. It can be used for creating the EFK stack or any other needed ElasticSearch services.
+
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "endpoint" {
-  description = "Domain-specific endpoint used to submit index, search, and data upload requests."
+  description = "Domain-specific HTTPS endpoint used to submit index, search, and data upload requests."
   value       = aws_elasticsearch_domain.es_domain.endpoint
 }
