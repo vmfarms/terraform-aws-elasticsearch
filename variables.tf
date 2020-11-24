fa211ea353289311c39a43d222dfda0c9253b00f
daniel <daniel@vmfarms.com>
2020-11-24 14:59:13 -0500

    fixing values to original settings

--- variables.tf
+++ variables.tf
@@ -45,7 +45,7 @@ variable "elasticsearch_version" {
 
 variable "create_iam_service_linked_role" {
   type        = string
-  default     = false
+  default     = true
   description = "Create an AWS Service-Linked Role for use by Elasticsearch. The service linked role is used to provide the Elasticsearch cluster with the appropriate permissions to run. This should be 'true' for the first Elasticsearch cluster you create, and 'false' thereafter. (Only one service-linked role can be created per AWS account and it is shared by all ES domains.) More info at https://docs.aws.amazon.com/IAM/latest/UserGuide/using-service-linked-roles.html"
 }
 
@@ -57,7 +57,7 @@ variable "encrypt_at_rest" {
 
 variable "node_to_node_encryption" {
   type        = string
-  default     = false
+  default     = true
   description = "Whether or not to use node-node encryption for the newly created ES domain. Requires `elasticsearch_version` version >= 6"
 }
 
@@ -93,34 +93,41 @@ variable "dedicated_master_type" {
 
 variable "log_publishing_options" {
   type        = bool
+  default     = true
   description = "Options for publishing slow and application logs to CloudWatch Logs. This block can be declared multiple times, for each log_type, within the same resource."
 }
 
 variable "log_type_index_slow_logs" {
   type        = string
+  default     = "INDEX_SLOW_LOGS"
   description = "A type of Elasticsearch log. Valid values: INDEX_SLOW_LOGS"
 }
 
 variable "log_type_search_slow_logs" {
   type        = string
+  default     = "SEARCH_SLOW_LOGS"
   description = "A type of Elasticsearch log. Valid values: SEARCH_SLOW_LOGS"
 }
 variable "log_type_es_application_logs" {
   type        = string
+  default     = "ES_APPLICATION_LOGS"
   description = "A type of Elasticsearch log. Valid values: ES_APPLICATION_LOGS"
 }
 variable "log_type_audit_logs" {
   type        = string
+  default     = "AUDIT_LOGS"
   description = "A type of Elasticsearch log. Valid values: AUDIT_LOGS"
 }
 
 variable "advanced_security_options" {
   type        = bool
+  default     = true
   description = "Whether advanced security is enabled"
 }
 
 variable "domain_endpoint_options" {
   type        = bool
+  default     = true
   description = "Whether or not to require HTTPS"
 }
 
